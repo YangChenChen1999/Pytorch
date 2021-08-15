a40a1e33e05a3067f590d1e8623a3aeaf09432aa
chenchen <1542332062@qq.com>
2021-08-15 22:01:20 +0800

    修改了main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
 
-num=201
-input_video = r'D:/project/spilt_pic/video/3/'+str(num)+'.mp4'
-output_dir = r'D:/project/spilt_pic/video/3/img/'
+
+input_video = r'C:/Users/CHENCHEN/PycharmProjects/Data/videos/89/89_1.mp4'
+output_dir = r'C:/Users/CHENCHEN/PycharmProjects/Data/picture/'
 
 
 START_TIME = 0  # 设置开始时间(单位秒)
-END_TIME = 1797  # 设置结束时间(单位秒)
+END_TIME = 1305  # 设置结束时间(单位秒)
 
 vidcap = cv2.VideoCapture(input_video)
 
@@ -27,7 +27,7 @@ count = 0
 seconds = 1
 while success and frametoStop >= count:
     if count % (fps * seconds) == 0:  # 每second秒保存一次
-        save_path = output_dir +str(num)+'_'+ str(count) + ".jpg"
+        save_path = output_dir + '89_1' + str(count) + ".jpg"
         cv2.imwrite(save_path, image)  # 保存图片
         print('Process %dth seconds: ' % int(count / (fps * seconds)), success)
     success, image = vidcap.read()  # 每次读取一帧
